f9aea3477953a1c959abcd18a96fb8d0b42a6c86
Chirag Dhingra <46193115+dhingrachirag@users.noreply.github.com>
2026-03-06 12:49:39 +0530

    Create config.py

---
+++ CustomLibs/config.py
@@ -0,0 +1,21 @@
+# MAINTAINER <Chirag Dhingra> <cdhingra@akamai.com>
+
+# condition to send email if 'True' email will be sent
+SEND_EMAIL = True
+
+# email smtp (smpt of yahoo, gmail, msn, outlook etc.,)
+SMPT = "email.msg.corp.akamai.com:25"
+
+# email subject
+SUBJECT = "Health Check Automation Execution Status"
+
+# credentials
+FROM = "QA_SRE_Automation@akamai.com"
+# PASSWORD = ""
+
+# receivers
+TO = "dl-sre-qa@akamai.com"
+CC = "sreqa-aaaahrsbtpuzpxvlg7czv5wlby@akamai.org.slack.com"
+
+COMPANY_NAME = "Asavie Health Check Demo"
+
